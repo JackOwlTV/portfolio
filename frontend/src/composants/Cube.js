3c213464157a07f2582cbade98ca67185d1ad669
JackOwl <dylan.vivant@sfr.fr>
2023-08-30 03:53:27 +0200

    Start carrousel

---
+++ frontend/src/composants/Cube.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import '../styles/composants/cube.css'
+
+
+export default function Cube(props) {
+    return (
+        <i onClick={props.onClick} className={props.className}></i>
+    )
+}
--- frontend/src/pages/Project.js
+++ frontend/src/pages/Project.js
@@ -1,7 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Cube from '../composants/Cube'
+import Carrousel from '../composants/Carrousel'
+import { projectList } from '../utils/projectList'
 
 export default function Project() {
+    const [activeBanner, setActiveBanner] = useState(0);
+
+    const previousCover = () => {
+        setActiveBanner((activeBanner - 1 + projectList.length) % projectList.length);
+    }
+
+    const nextCover = () => {
+        setActiveBanner((activeBanner + 1) % projectList.length);
+    }
+
     return (
-        <div>Project</div>
+        <div>
+            <div className="cube">
+                <Cube onClick={previousCover}
+                    className={
+                        projectList.length > 1
+                            ? "fa-solid fa-stop"
+                            : null
+                    } />
+                <Cube onClick={nextCover}
+                    className={
+                        projectList.length > 1
+                            ? "fa-solid fa-stop"
+                            : null
+                    } />
+            </div>
+
+            <section>
+                {projectList.map((project, index) => (
+                    <Carrousel
+                        className={`img-banner ${index === activeBanner ? "active-banner" : ""}`}
+                        key={index}
+                        cover={project.cover}
+                    />
+                ))}
+            </section>
+        </div>
     )
 }
