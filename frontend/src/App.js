aebb0207d1f087aa3797aecedbf04649c14ff267
JackOwl <dylan.vivant@sfr.fr>
2023-08-30 03:05:34 +0200

    Debuggage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Routes, Route } from "react-router-dom";
 import Navbar from './composants/Navbar';
 import Home from "./pages/Home";
@@ -6,19 +6,10 @@ import Project from "./pages/Project";
 import School from "./pages/School";
 import Work from "./pages/Work";
 import Workmore from "./pages/Workmore";
-import Connect from "./pages/Connect";
-import Addproject from "./pages/Addproject";
-import { useUser } from './composants/lib/customHook';
-import { APP_ROUTES } from './utils/constants';
 import './styles/App.css';
 
 function App() {
-  const [user, setUser] = useState(null);
-  const { connectedUser } = useUser();
 
-  useEffect(() => {
-    setUser(connectedUser);
-  }, [connectedUser]);
   return (
     <div className="App">
       <Navbar />
@@ -28,8 +19,6 @@ function App() {
         <Route path="/school" element={<School />} />
         <Route path="/work" element={<Work />} />
         <Route path="/work/http" element={<Workmore />} />
-        <Route path="/admin" element={<Connect />} />
-        <Route path="/admin/add_project" element={<Addproject />} />
 
       </Routes>
 
