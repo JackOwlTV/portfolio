faaf922a2a59d1539ebfd8ce7c200910eeeb4778
JackOwl <dylan.vivant@sfr.fr>
2023-08-30 03:02:34 +0200

    Suppression du backend & ajout d'une list.js

--- frontend/src/composants/Cube.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import '../styles/composants/cube.css'
-
-export default function Cube(props) {
-    return (
-        <i onClick={props.onClick} className={props.className}></i>
-    )
-}
--- frontend/src/composants/ProjectBanner.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import '../styles/composants/projectbanner.css'
-export default function ProjectBanner(props) {
-    return (
-        <img className={props.className} src={props.imgSrc} alt={props.title} />
-    )
-}
